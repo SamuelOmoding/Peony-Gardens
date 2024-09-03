f469f847d15ee4b54c88f46c715c9a60c147aabd
Sammy-O <156414617+SamuelOmoding@users.noreply.github.com>
2024-09-03 12:23:54 +0300

    Delete my-app directory

--- my-app/src/components/DealsOffers.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DealsOffers() {
-  return (
-    <div>DealsOffers</div>
-  )
-}
-
-export default DealsOffers
--- my-app/src/components/ExperienceActivities.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ExperienceActivities() {
-  return (
-    <div>ExperienceActivities</div>
-  )
-}
-
-export default ExperienceActivities
